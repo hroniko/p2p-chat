cd12b07dcb2342cbfdc12bbf99876eabe5562c28
Anatolii Bedarev <anatolii.bedarev@netcracker.com>
2026-04-07 14:26:39 +0300

    v1 :: try

--- src/main/java/com/chat/p2p/P2PChatApplication.java
+++ src/main/java/com/chat/p2p/P2PChatApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// start
 @SpringBootApplication
 @EnableScheduling
 public class P2PChatApplication {
